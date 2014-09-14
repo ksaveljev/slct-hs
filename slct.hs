db71c113b93723e1d57c7d3c6d76123fca5bf44f
Dmitri Melnikov <github@dmit.me>
2014-09-14 11:43:27 +0300

    Switch hashing function to FNV-1a and Data.Map to Data.IntMap

--- slct.hs
+++ slct.hs
@@ -3,27 +3,28 @@
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as C
-import Data.Map.Strict (Map)
-import qualified Data.Map.Strict as Map
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as Map
 import Data.List (foldl')
-import Data.Word (Word32, Word64)
+import Data.Word (Word32)
 import System.IO (Handle, hIsEOF, stdin)
 import Options.Applicative
 
-import SLCT.Constants
 import SLCT.Options
 import qualified SLCT.ByteStringHash as BSHash
 
--- TODO: consider a hash with better distribution
-hash :: ByteString -> Word64
+type Hash = Int
+type HashCount = IntMap Word32
+
+hash :: ByteString -> Hash
 {-# INLINE hash #-}
-hash = BSHash.shiftAddXor
+hash = fromIntegral . BSHash.fnv1a64
 
 splitWords :: ByteString -> [ByteString]
 {-# INLINE splitWords #-}
 splitWords = C.split ' '
 
-populate :: Map Word64 Word32 -> Handle -> IO (Map Word64 Word32)
+populate :: HashCount -> Handle -> IO HashCount
 populate hashes h = do
     eof <- hIsEOF h
     if eof then return hashes
@@ -33,14 +34,9 @@ populate hashes h = do
              let hashes' = foldl' (\m w -> Map.insertWith (+) (hash w) 1 m) hashes ws
              populate hashes' h
 
--- TODO: remove it, temporary for testing
-printOptions :: Options -> IO()
-printOptions o = do
-    putStrLn $ show o
-
 main :: IO()
 main = do
-    printOptions =<< execParser (optionsParser `withInfo` "SLCT-hs version 0.1.0.0, Copyright AUTHORS")
+    print =<< execParser (optionsParser `withInfo` "SLCT-hs version 0.1.0.0, Copyright AUTHORS")
     m <- populate Map.empty stdin
     putStr $ show $ Map.size m
     where
